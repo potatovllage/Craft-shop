0bdefd97d9ad2f0a42203909f60e82b114e7855c
Yeong joon <bluehome8626@naver.com>
2023-09-18 11:35:54 +0900

    뱀 움직이기

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,17 +1,17 @@
-//X축 방향
+// X축 방향
 let X_MOVE = 0;
 
-//Y축 방향
+// Y축 방향
 let Y_MOVE = 1;
 
 // 테이블 크기
 let tableTileSize = 21;
 
-//뱀
-let snake = new Array();
-
-//사과
-let apple = new Array();
+// 숫자 랜덤 생성
+function randomNum(min, max) {
+  var ranNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  return ranNum;
+}
 
 // 테이블 생성 함수
 function tableSetUp() {
@@ -30,8 +30,46 @@ function tableSetUp() {
   document.getElementById("snakeGamingZone").innerHTML = tableCode;
 }
 
-//뱀 그리기
-function drawSnake() {
+// 사과
+let apple = new Array();
+
+//사과 초기화
+function appleSetUp() {
+  let x;
+  let y;
+
+  do {
+    x = randomNum(0, tableTileSize - 1);
+    y = randomNum(0, tableTileSize - 1);
+  } while (
+    document.getElementById("tile" + x + "_" + y).classList.contains("snake")
+  );
+
+  apple = [];
+  apple.push([x, y]);
+  drawApple();
+}
+
+//사과 생성
+function drawApple() {
+  let apples = document.getElementsByClassName("apple");
+
+  while (apples.length) {
+    apples[0].classList.remove("apple");
+  }
+
+  for (let i = 0; i < apple.length; i++) {
+    document
+      .getElementById("tile" + apple[i][0] + "_" + apple[i][1])
+      .classList.add("apple");
+  }
+}
+
+// 뱀 배열
+let snake = new Array();
+
+// 뱀 그리기
+function createSnake() {
   let score = parseInt(document.getElementById("score").innerHTML);
 
   let state = "";
@@ -41,9 +79,7 @@ function drawSnake() {
     snakes[0].classList.remove("snake");
   }
 
-  let snakeLength = snake.length;
-
-  for (let i = 0; i < snakeLength; i++) {
+  for (let i = 0; i < snake.length; i++) {
     document
       .getElementById("tile" + snake[i][0] + "_" + snake[i][1])
       .classList.add("snake");
@@ -53,7 +89,7 @@ function drawSnake() {
         .classList.contains("apple")
     ) {
       score = score + 100;
-      document.getElementById("score").innerHTML = score; //점수 반영
+      document.getElementById("score").innerHTML = score;
       apple.pop();
       initApple();
 
@@ -67,7 +103,7 @@ function drawSnake() {
 function snakeSetUp() {
   snake = [];
   snake.push([0, 1]);
-  drawSnake();
+  createSnake();
 }
 
 // 게임 세팅
@@ -77,6 +113,7 @@ function gameSetUp() {
 
   tableSetUp();
   snakeSetUp();
+  appleSetUp();
 }
 
 // 게임 시작
@@ -84,5 +121,76 @@ function handleStartButton() {
   document.getElementsByClassName("startButton")[0].style.display = "none";
   document.getElementsByClassName("start")[0].style.display = "none";
 
+  setInterval(function () {
+    handleKeyPress();
+    moveSnake();
+  }, 100);
   gameSetUp();
+}
+
+// 키보드 감지
+function handleKeyPress() {
+  window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+      case "ArrowUp":
+        moveUp();
+        break;
+      case "ArrowDown":
+        moveDown();
+        break;
+      case "ArrowLeft":
+        moveLeft();
+        break;
+      case "ArrowRight":
+        moveRight();
+        break;
+    }
+  });
+}
+
+// 뱀 움직이기
+function moveSnake() {
+  let headX = snake[0][0];
+  let headY = snake[0][1];
+
+  let newHeadX = headX + X_MOVE;
+  let newHeadY = headY + Y_MOVE;
+
+  snake.unshift([newHeadX, newHeadY]);
+
+  snake.pop();
+
+  createSnake();
+}
+
+// 위쪽
+function moveUp() {
+  if (X_MOVE == 0) {
+    X_MOVE = -1;
+    Y_MOVE = 0;
+  }
+}
+
+// 아래쪽
+function moveDown() {
+  if (X_MOVE == 0) {
+    X_MOVE = 1;
+    Y_MOVE = 0;
+  }
+}
+
+// 왼쪽
+function moveLeft() {
+  if (Y_MOVE == 0) {
+    X_MOVE = 0;
+    Y_MOVE = -1;
+  }
+}
+
+// 오른쪽
+function moveRight() {
+  if (Y_MOVE == 0) {
+    X_MOVE = 0;
+    Y_MOVE = 1;
+  }
 }
