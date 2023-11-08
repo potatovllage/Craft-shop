b2b9bd19d5dae7e75164e26527cc092e362e4746
Yeong joon <bluehome8626@naver.com>
2023-11-08 12:58:23 +0900

    feat(Flip-The-Card): setting image file

---
+++ Flip-the-Card/src/assets/index.ts
@@ -0,0 +1,8 @@
+export { default as blueFlower } from "./blueFlower.png";
+export { default as boom } from "./boom.png";
+export { default as goomba } from "./goomba.png";
+export { default as green } from "./green.png";
+export { default as greenShell } from "./greenShell.png";
+export { default as red } from "./red.png";
+export { default as redFlower } from "./redFlower.png";
+export { default as redShell } from "./redShell.png";
---
+++ Flip-the-Card/src/config/index.ts
@@ -0,0 +1,96 @@
+import {
+  blueFlower,
+  boom,
+  goomba,
+  green,
+  greenShell,
+  red,
+  redFlower,
+  redShell,
+} from "../assets/index";
+import { CardProps } from "../types";
+
+export const cardArray: CardProps[] = [
+  {
+    id: 1,
+    image: blueFlower,
+    isOpen: false,
+  },
+  {
+    id: 2,
+    image: boom,
+    isOpen: false,
+  },
+
+  {
+    id: 3,
+    image: goomba,
+    isOpen: false,
+  },
+  {
+    id: 4,
+    image: green,
+    isOpen: false,
+  },
+  {
+    id: 5,
+    image: greenShell,
+    isOpen: false,
+  },
+  {
+    id: 6,
+    image: red,
+    isOpen: false,
+  },
+  {
+    id: 7,
+    image: redFlower,
+    isOpen: false,
+  },
+  {
+    id: 8,
+    image: redShell,
+    isOpen: false,
+  },
+  {
+    id: 1,
+    image: blueFlower,
+    isOpen: false,
+  },
+  {
+    id: 2,
+    image: boom,
+    isOpen: false,
+  },
+
+  {
+    id: 3,
+    image: goomba,
+    isOpen: false,
+  },
+  {
+    id: 4,
+    image: green,
+    isOpen: false,
+  },
+  {
+    id: 5,
+    image: greenShell,
+    isOpen: false,
+  },
+  {
+    id: 6,
+    image: red,
+    isOpen: false,
+  },
+  {
+    id: 7,
+    image: redFlower,
+    isOpen: false,
+  },
+  {
+    id: 8,
+    image: redShell,
+    isOpen: false,
+  },
+];
